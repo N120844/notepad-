4b9c6359f4342b6b723fced72fd44185793c1cd5
N120844 <raghujanaiiitn@gmail.com>
2017-02-19 15:49:08 +0530

    intial commit

---
+++ prime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int n,i;
+scanf("%d",&n);
+int c=0;
+for(i=1;i<=n;i++){
+  if(n%i==0)
+   {c++;}
+ }
+if(c==2)
+printf("prime\n");
+else
+printf("not prime\n");
+return 0;
+}
